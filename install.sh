d68672c34295eab92d36d71b7d6ac303114018cc
Connor Holyday <connorholyday@gmail.com>
2018-04-21 13:34:27 +0100

    Updated install script with symlinks

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./brew
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
+
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.config/nvim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s "`pwd`"/vimrc ../.vimrc
+ln -s "`pwd`"/vimrc ../.config/nvim/init.vim
+ln -s "`pwd`"/zshrc ../.zshrc
+ln -s "`pwd`"/gitconfig ../.gitconfig
+/usr/local/bin/nvim +PlugInstall +qall
