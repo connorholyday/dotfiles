c942a63a89202a145a25569714303de3b4a5aded
Connor Holyday <connor.holyday@rareloop.com>
2018-07-02 12:12:23 +0100

    Fix ctags symlink and pwd

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ ln -s "`pwd`"/hyper.js ~/.hyper.js
 ln -s "`pwd`"/chunkwmrc ~/.chunkwmrc
 ln -s "`pwd`"/skhdrc ~/.skhdrc
 ln -s "`pwd`"/ripgreprc ~/.ripgreprc
-ln -s "`pwd`"/hooks/ ~/.git_template/hooks/
+ln -s "`pwd`"/hooks/ ~/.git_template/
 ln -s "`pwd`"/ctags ~/.ctags
 
 chmod +x ~/.chunkwmrc
